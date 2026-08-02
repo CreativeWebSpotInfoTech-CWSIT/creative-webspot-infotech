9cf587ab76f6291af090bbdcd88a03961920fdac
ParthibanR <parthibanvijay467@gmail.com>
2026-08-02 20:45:55 +0530

    MODIFIED: vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     plugins: [react()],
-    base: '/creative-webspot-infotech/',
-});
+    base: mode === "production" ? "/creative-webspot-infotech/" : "/",
+}));
